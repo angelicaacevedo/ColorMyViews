3c5cf8ed79f680e587a02a25f269458190b2fd06
Angélica Acevedo <aceroacevedoangelica@gmail.com>
2020-10-29 20:08:58 -0300

    Botões definidos

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.colormyviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var currentColor = R.color.grey
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setColorButtonsAction()
+    }
+
+
+    fun setColorButtonsAction() {
+        button_red.setOnClickListener {
+            currentColor = R.color.red
+        }
+
+        button_yellow.setOnClickListener {
+            currentColor = R.color.yellow
+        }
+
+        button_green.setOnClickListener {
+            currentColor = R.color.green
+        }
+
     }
 }
+
+
+
